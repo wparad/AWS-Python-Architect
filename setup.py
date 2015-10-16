89bd1080d6187e8da12e9fd83eb4ce3a5e4d5712
Warren Parad <wparad@gmail.com>
2015-10-16 08:57:13 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'aws-architect',
+  packages = ['aws-architect'], # this must be the same as the name above
+  version = '0.0',
+  description = 'AWS Architect is used to configure and deploy AWS-based microservices.',
+  author = 'Warren Parad',
+  author_email = 'wparad@gmail.com',
+  url = 'https://github.com/wparad/AWS-Architect.git', # use the URL to the github repo
+  download_url = 'https://github.com/wparad/AWS-Architect/tarball/0.0',
+  keywords = ['aws', 'cloudfront', 'package creation', 'lambda', 'api gateway'], # arbitrary keywords
+  classifiers = [],
+)
